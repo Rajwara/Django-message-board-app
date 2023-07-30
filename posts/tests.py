c3f0bebba8c713b6547ade7246f3a18cc550d92d
Rajwara <maliksaadsultan766@gmail.com>
2023-07-31 00:08:43 +0500

    test intialize

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import post
 # Create your tests here.
+
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = post.objects.create(text = "This is a ourtest!")
+        
+        
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "Tis is other test")
+        
